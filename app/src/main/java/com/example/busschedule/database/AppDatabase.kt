2220d975ccf13f60ddd26881b30602d9bf6ca883
bsh6463 <bsh9510@gmail.com>
2022-03-12 09:48:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/busschedule/database/AppDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.busschedule.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.busschedule.database.schedule.Schedule
+import com.example.busschedule.database.schedule.ScheduleDao
+
+@Database(entities = arrayOf(Schedule::class), version = 1)
+abstract class AppDatabase:RoomDatabase() {
+
+    abstract fun scheduleDao(): ScheduleDao
+
+
+    companion object{
+        //companion object에 선언한 field 및 method 는 static 과 동일
+        //Appdatabase 인스턴스 없이 사용 가능.
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context:Context): AppDatabase{
+            return INSTANCE?: synchronized(this){
+                val instance = Room.databaseBuilder(context,
+                AppDatabase::class.java, "app_database")
+                    .createFromAsset("databse/bus_schedule.db")
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
@@ -0,0 +1,14 @@
+package com.example.busschedule.database.schedule
+
+import androidx.room.Dao
+import androidx.room.Query
+
+@Dao //데이터 읽고 조작하는 함수 포함.
+interface ScheduleDao {
+
+    @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
+    fun getAll(): List<Schedule>
+
+    @Query("select * from schedule where stop_name = :stopName order by arrival_time ASC")
+    fun getByStopName(stopName: String) : List<Schedule>
+}
